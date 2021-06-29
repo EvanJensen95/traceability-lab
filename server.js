dcde56548bcd671c8d1eb787981e115d97d76d12
Evan <evanjensen95@gmail.com>
2021-06-29 13:10:12 -0700

    second

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: '4d36f513a6324d479b0c487be412d066',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
 app.use(express.json())
 
 
 
-// app.get('/', function(req, res){
-//     rollbar.log('hello world')
-//     res.sendFile(path.join(__dirname, '/public/index.html'))
-// })
+app.get('/', function(req, res){
+    rollbar.log('hello world')
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 
-const port = process.env.PORT || 4545;
+const port = process.env.PORT || 5040;
 app.listen(port, function(){ console.log(`Server is rocking out on ${port}`)})
